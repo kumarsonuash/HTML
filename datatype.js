38c8a3f4cbf24cc98bf31dae48815e8defd3d726
Ashutosh kumar sonu <iamashutoshthakur143@gmail.com>
2024-12-20 05:40:52 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version 
+
+// alert("3+3") we are using nodejs not browser 
+
+console.log(3+3)
+// code Readablity should be heigh
+console.log ("Ashu")
+/*
+   DATA-TYPE 
+String
+Number
+Bigint
+Boolean
+Undefined =
+Null =
+Symbol ==> unique 
+Object ==>
+
+
+*/
+let age = 18
+let isLoggedIn =false
+let state; 
+console.log (typeof undefined );//output will be undefined
+console.log(typeof null); //object 
+  
